998e53228fb300ce30b06b2c3b2b33ec918b7ec6
Senthil Padmanabhan <senthil.padmanabhan@yahoo.com>
2019-06-03 17:16:14 -0700

    Fixing security vulnerability

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         babel: {
             options: {
-                optional: ['runtime']
             },
             all: {
                 files: [{
